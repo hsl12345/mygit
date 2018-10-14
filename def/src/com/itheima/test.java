78a8b69cfab24750fc7913d43723a46e7c9f4d36
hsl12345 <hsl1234567>
2018-10-14 20:09:11 +0800

    修改本地工程添加到git

--- def/src/com/itheima/test.java
+++ def/src/com/itheima/test.java
@@ -4,4 +4,8 @@ public class test {
     public static void main(String[] args) {
         System.out.println("22222222222222222");
     }
+    public void test(){
+        int a=2;
+        a+=10;
+    }
 }
